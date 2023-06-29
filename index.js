9747cfb9e3b60e15c6f9a22e34313f9019d5b57b
Jorge Garcia <jegcelorio@yahoo.com>
2023-06-28 20:28:17 -0500

    Added index.js with selectors

---
+++ index.js
@@ -0,0 +1,9 @@
+const rememberDiv = document.querySelector(".remember");
+const forgetDiv = document.querySelector(".forget");
+const form = document.querySelector("form");
+const nameInput = document.querySelector("#entername");
+const submitBtn = document.querySelector("#submitname");
+const forgetBtn = document.querySelector("forgetname");
+
+const h1 = document.querySelector("h1");
+const personalGreeting = document.querySelector(".personal-greeting");
